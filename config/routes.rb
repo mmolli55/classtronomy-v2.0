e039d045397c49207944014c783c6dc3e934256b
mmolli55 <matthewjosephmolli@gmail.com>
2014-12-12 12:19:07 -0500

    added user_name field to Users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root 'welcome#index'
+
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
