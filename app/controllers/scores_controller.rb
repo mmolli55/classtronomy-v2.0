358c2e0ec51febfd2a72fb05478dfdce989a1088
mmolli55 <matthewjosephmolli@gmail.com>
2014-12-17 14:59:57 -0500

    list items now uniform size

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,11 +2,7 @@ class ScoresController < ApplicationController
   def update
     course_score = current_user.scores.find(params[:id])
     course_score.update(score: params[:score])
-    respond_to do |format|
-      # format.html { redirect_to current_user }
-      # format.json { render nothing: true, status: 200 }
-      render nothing: true, status: 200
-    end
+    render nothing: true, status: 200
   end
 
 end
